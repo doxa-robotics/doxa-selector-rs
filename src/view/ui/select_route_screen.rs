b7f86dc6c6d4e912d35f1706f89c519929da412c
zabackary <137591653+zabackary@users.noreply.github.com>
2026-01-01 22:56:43 +0900

    feat: add back button

--- src/view/ui/select_route_screen.rs
+++ src/view/ui/select_route_screen.rs
@@ -1,20 +1,38 @@
-use buoyant::view::View;
+use buoyant::view::{prelude::ViewModifier, EmptyView, VStack, View, ZStack};
 
-use crate::view::{color, AppState};
+use crate::view::{
+    color,
+    ui::button::{self, ButtonStyle},
+    AppState,
+};
 
 pub fn select_route_screen(
     data: &crate::view::AppData,
     category_index: usize,
 ) -> impl View<color::Color, AppState> + use<'_> {
     let routes = &data.route_names_map[&category_index];
-    super::selector::selector(
-        "Select route",
-        routes,
-        move |state: &mut AppState, route_index: usize| {
-            let global_route_index = routes[route_index].2;
-            println!("Selected route index: {}", global_route_index);
-            state.external.borrow_mut().selection = global_route_index;
-            state.screen = crate::view::ui::Screen::ConfirmSelection;
-        },
-    )
+
+    ZStack::new((
+        VStack::new((
+            button::button("Back", ButtonStyle::default(), |state: &mut AppState| {
+                state.screen = crate::view::ui::Screen::SelectCategory
+            })
+            .padding(buoyant::view::prelude::Edges::All, 8),
+            EmptyView,
+        ))
+        .flex_frame()
+        .with_infinite_max_height()
+        .with_infinite_max_width()
+        .with_alignment(buoyant::layout::Alignment::TopLeading),
+        super::selector::selector(
+            "Select route",
+            routes,
+            move |state: &mut AppState, route_index: usize| {
+                let global_route_index = routes[route_index].2;
+                println!("Selected route index: {}", global_route_index);
+                state.external.borrow_mut().selection = global_route_index;
+                state.screen = crate::view::ui::Screen::ConfirmSelection;
+            },
+        ),
+    ))
 }
