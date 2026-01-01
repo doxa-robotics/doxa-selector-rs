072fd40ff9c80f902b1354a39b75decff6ec19d5
zabackary <137591653+zabackary@users.noreply.github.com>
2026-01-01 20:07:49 +0900

    refactor: optimize calibration overlay

--- src/view/ui/calibrating_overlay.rs
+++ src/view/ui/calibrating_overlay.rs
@@ -1,42 +1,31 @@
 use std::time::Duration;
 
-use buoyant::{render::Rect, view::prelude::*};
-use vexide::prelude::Display;
+use buoyant::{transition::Move, view::prelude::*};
 
-use crate::view::{
-    color, spacing,
-    ui::{
-        button::{self, ButtonStyle},
-        AppState,
-    },
-};
+use crate::view::{color, spacing, ui::AppState};
 
 pub fn calibrating_overlay(state: &AppState) -> impl View<color::Color, AppState> {
-    // TODO: we always render this, even when not calibrating. Optimize?
     let calibrating = state.external.borrow().calibrating;
-    RoundedRectangle::new(32)
-        .foreground_color(color::M3_ERROR_CONTAINER)
-        .padding(Edges::All, 48)
-        .overlay(
-            Alignment::Center,
-            VStack::new((
-                Text::new("Calibrating...", &*crate::view::font::HEADING)
-                    .foreground_color(color::M3_ON_ERROR_CONTAINER)
-                    .hint_background_color(color::M3_ERROR_CONTAINER),
-                Text::new("Please do not move the robot.", &*crate::view::font::BODY)
-                    .foreground_color(color::M3_ON_SURFACE_VARIANT)
-                    .hint_background_color(color::M3_ERROR_CONTAINER),
-            ))
-            .with_spacing(spacing::ELEMENT),
-        )
-        .offset(
-            0,
-            if calibrating {
-                0
-            } else {
-                Display::VERTICAL_RESOLUTION as i32
-            },
-        )
+    calibrating
+        // If calibrating is true, then render
+        .then(|| {
+            RoundedRectangle::new(32)
+                .foreground_color(color::M3_ERROR_CONTAINER)
+                .padding(Edges::All, 48)
+                .overlay(
+                    Alignment::Center,
+                    VStack::new((
+                        Text::new("Calibrating...", &*crate::view::font::HEADING)
+                            .foreground_color(color::M3_ON_ERROR_CONTAINER)
+                            .hint_background_color(color::M3_ERROR_CONTAINER),
+                        Text::new("Please do not move the robot.", &*crate::view::font::BODY)
+                            .foreground_color(color::M3_ON_SURFACE_VARIANT)
+                            .hint_background_color(color::M3_ERROR_CONTAINER),
+                    ))
+                    .with_spacing(spacing::ELEMENT),
+                )
+                .transition(Move::bottom())
+        })
         .animated(
             Animation::ease_in_out(Duration::from_millis(800)),
             calibrating,
